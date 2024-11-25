f4393ac7918624f03c6a8fcde37a8b1c8c3c426d
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-26 01:11:36 +0600

    Added vercel json file

--- api/index.go
+++ api/index.go
@@ -1,10 +1,30 @@
 package handler
  
-import (
-  "fmt"
-  "net/http"
+import ( 
+    "encoding/json"
+    "net/http"
 )
- 
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
+type Person struct { 
+    Name string `json:"name"` 
+    Age int `json:"age"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) { 
+    person := Person{  Name: "John",  Age: 30 } 
+
+    // Encoding - 2 step . NewEncoder and Encode
+    encoder := json.NewEncoder(w) 
+
+    err := encoder.Encode(person) 
+
+    if err != nil {  
+        http.Error(w, err.Error(), http.StatusInternalServerError)  
+
+        return 
+   }}
+
+func Main() { 
+   http.HandleFunc("/", Handler) 
+   http.ListenAndServe(":8080", nil)
 }
