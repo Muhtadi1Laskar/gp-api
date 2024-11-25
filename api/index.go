cbe9a5980ace748d47f3fa42e7e1da962ff83afc
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-26 01:01:51 +0600

    Changed the code

--- api/index.go
+++ api/index.go
@@ -1,23 +1,10 @@
 package handler
-
+ 
 import (
-	"encoding/json"
-	"net/http"
+  "fmt"
+  "net/http"
 )
-
-type Person struct {
-	Name string `json:"name"`
-	ID int `json:"id"`
-}
-
+ 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	person := Person{ Name: "Gino", ID: 343242543242 }
-
-	jsonStr, err := json.Marshal(person)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonStr)
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
