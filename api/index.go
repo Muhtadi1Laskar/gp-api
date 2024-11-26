0e479753d0732d32621d9d61fd34a7be8cea0007
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-26 13:26:13 +0600

    Added Handlers file

--- api/index.go
+++ api/index.go
@@ -5,7 +5,11 @@ import (
 	"net/http"
 )
 
+func HashHandler(w http.ResponseWriter, r *http.Request) {
+	handlers.HashData(w, r)
+}
+
 func Main() {
-	http.HandleFunc("/", handlers.HashData)
+	http.HandleFunc("/", HashHandler)
 	http.ListenAndServe(":8080", nil)
 }
