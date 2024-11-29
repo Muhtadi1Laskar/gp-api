f0f2bc9ebbbc689aec3fd8efba960c6e1be40082
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-29 06:07:51 +0600

    Changed the caeser cipher code

--- Ciphers/caeser_cipher.go
+++ Ciphers/caeser_cipher.go
@@ -1,21 +1,30 @@
 package ciphers
 
-func EncryptCaeser(data string, key int) string {
-	var result []rune
+import (
+	"strings"
+	"unicode"
+)
 
+func EncryptCaeser(data string, key int) string {
+	var result strings.Builder
 	key = key % 26
 	if key < 0 {
-		key += 26
-	}
+			key += 26
+		}
 
 	for _, char := range data {
-		if char >= 'A' && char <= 'Z' {
-			result = append(result, rune('A'+(char-'A'+rune(key))%26))
-		} else if char >= 'a' && char <= 'z' {
-			result = append(result, rune('a'+(char-'a'+rune(key))%26))
+		if unicode.IsLetter(char) {
+			offset := 'A'
+			if unicode.IsLower(char) {
+				offset = 'a'
+			}
+			// Perform the shift and wrap around the alphabet.
+			newChar := (char-offset+rune(key)+26)%26 + offset
+			result.WriteRune(newChar)
 		} else {
-			result = append(result, char)
+			// Leave non-alphabetic characters as they are.
+			result.WriteRune(char)
 		}
 	}
-	return string(result)
+	return result.String()
 }
