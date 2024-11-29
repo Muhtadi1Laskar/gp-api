23bebdff4b5f67a86fd57a7319f98abc856397d9
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-29 23:12:58 +0600

    Added the bacon cipher

---
+++ Ciphers/bacon_cipher.go
@@ -0,0 +1,60 @@
+package ciphers
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
+var codes = map[rune]string{'A': "aaaaa", 'B': "aaaab", 'C': "aaaba", 'D': "aaabb", 'E': "aabaa",
+	'F': "aabab", 'G': "aabba", 'H': "aabbb", 'I': "abaaa", 'J': "abaab",
+	'K': "ababa", 'L': "ababb", 'M': "abbaa", 'N': "abbab", 'O': "abbba",
+	'P': "abbbb", 'Q': "baaaa", 'R': "baaab", 'S': "baaba", 'T': "baabb",
+	'U': "babaa", 'V': "babab", 'W': "babba", 'X': "babbb", 'Y': "bbaaa", 'Z': "bbaab",
+}
+
+func getCode(key rune) string {
+	if value, ok := codes[key]; ok {
+		return value
+	}
+	return ""
+}
+
+func getLetter(str string) rune {
+	for key, value := range codes {
+		if value == str {
+			return key
+		}
+	}
+	return '#'
+}
+
+func BaconCipherEncrypt(data string) string {
+	var result strings.Builder
+	capitalizedWord := strings.ToUpper(data)
+
+	for _, c := range capitalizedWord {
+		if unicode.IsLetter(c) {
+			letter := getCode(c)
+			result.WriteString(letter + " ")
+		}
+	}
+	return result.String()
+}
+
+func BaconCipherDecrypt(data string) (string, error) {
+	var result strings.Builder
+	formattedStr := strings.ReplaceAll(data, " ", "")
+
+	if len(formattedStr) % 5 != 0 {
+		return "", errors.New("invalid input length")
+	}
+
+	for i := 0; i < len(formattedStr); i+= 5 {
+		substr := formattedStr[i : i+5]
+		letter := string(getLetter(substr))
+
+		result.WriteString(letter)
+	}
+	return result.String(), nil
+}
--- main.go
+++ main.go
@@ -2,35 +2,42 @@ package main
 
 import (
 	"fmt"
-	"go-api/handlers"
-	"net/http"
+	ciphers "go-api/Ciphers"
+	// "go-api/handlers"
+	// "net/http"
 )
 
-func enableCORS(h http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+// func enableCORS(h http.Handler) http.Handler {
+//     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//         w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+//         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+//         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
+//         if r.Method == "OPTIONS" {
+//             w.WriteHeader(http.StatusOK)
+//             return
+//         }
 
-        h.ServeHTTP(w, r)
-    })
-}
+//         h.ServeHTTP(w, r)
+//     })
+// }
 
 func main() {
-	mux := http.NewServeMux()
+	var plainText string = "1H2e3l4l5o6 $S%E@x!y! "
+	var cipherText string = ciphers.BaconCipherEncrypt(plainText)
+	decryptedText, _ := ciphers.BaconCipherDecrypt(cipherText)
 
-	mux.HandleFunc("/hash", handlers.HashData)
-	mux.HandleFunc("/aes/encrypt", handlers.CipherAES)
-	mux.HandleFunc("/cipher/xor", handlers.CipherXOR)
-	mux.HandleFunc("/cipher/ceaserCipher", handlers.CeaserCipherHandler)
-	mux.HandleFunc("/cipher/vignereCipher", handlers.VigenereCipherHandler)
-	mux.HandleFunc("/cipher/atbashCipher", handlers.AtbashHandler)
-	mux.HandleFunc("/cipher/rot13Cipher", handlers.Rot13Handler)
-	fmt.Println("Server running on 5000")
-	http.ListenAndServe(":5000", enableCORS(mux))
+	fmt.Println(cipherText)
+	fmt.Println(decryptedText)
+	// mux := http.NewServeMux()
+
+	// mux.HandleFunc("/hash", handlers.HashData)
+	// mux.HandleFunc("/aes/encrypt", handlers.CipherAES)
+	// mux.HandleFunc("/cipher/xor", handlers.CipherXOR)
+	// mux.HandleFunc("/cipher/ceaserCipher", handlers.CeaserCipherHandler)
+	// mux.HandleFunc("/cipher/vignereCipher", handlers.VigenereCipherHandler)
+	// mux.HandleFunc("/cipher/atbashCipher", handlers.AtbashHandler)
+	// mux.HandleFunc("/cipher/rot13Cipher", handlers.Rot13Handler)
+	// fmt.Println("Server running on 5000")
+	// http.ListenAndServe(":5000", enableCORS(mux))
 }
