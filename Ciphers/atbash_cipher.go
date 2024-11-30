9310027eeb78a03094b7c26ecea24a93a4ceb60b
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-30 19:19:14 +0600

    Changed the atbash cipher code to handle the case sensitive output

--- Ciphers/atbash_cipher.go
+++ Ciphers/atbash_cipher.go
@@ -8,18 +8,17 @@ import (
 func AtbashCipher(data string) string {
 	var result strings.Builder
 
-	for _, c := range data {
-		if unicode.IsLetter(c) {
-			var offset rune
-			if unicode.IsLower(c) {
-				offset = 'a'
+	for _, char := range data {
+		if unicode.IsLetter(char) {
+			var cipher rune
+			if unicode.IsUpper(char) {
+				cipher = 'Z' - char + 'A'
 			} else {
-				offset = 'A'
+				cipher = 'z' - char + 'a'
 			}
-			reversedRune := rune(offset + ('Z' - c))
-			result.WriteRune(reversedRune)
+			result.WriteRune(cipher)
 		} else {
-			result.WriteRune(c)
+			result.WriteRune(char)
 		}
 	}
 	return result.String()
