9536a1343e1d6aeb09eb89c4768f93f9d11cb776
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-02-24 15:03:49 +0600

    Refactored the hash function code

--- Hashes/hashes.go
+++ Hashes/hashes.go
@@ -19,20 +19,31 @@ import (
 
 func GetHashFunc() map[string]func() (hash.Hash, error) {
 	return map[string]func() (hash.Hash, error){
-		"md5":         func() (hash.Hash, error) { return md5.New(), nil },
-		"sha256":      func() (hash.Hash, error) { return sha256.New(), nil },
-		"sha1":        func() (hash.Hash, error) { return sha1.New(), nil },
-		"sha224":      func() (hash.Hash, error) { return sha256.New224(), nil },
-		"sha384":      func() (hash.Hash, error) { return sha512.New384(), nil },
-		"sha512_224":  func() (hash.Hash, error) { return sha512.New512_224(), nil },
-		"sha512":      func() (hash.Hash, error) { return sha512.New(), nil },
-		"ripemd160":   func() (hash.Hash, error) { return ripemd160.New(), nil },
-		"md4":         func() (hash.Hash, error) { return md4.New(), nil },
-		"sha3_224":    func() (hash.Hash, error) { return sha3.New224(), nil },
-		"sha3_256":    func() (hash.Hash, error) { return sha3.New256(), nil },
-		"sha3_384":    func() (hash.Hash, error) { return sha3.New384(), nil },
-		"sha3_512":    func() (hash.Hash, error) { return sha3.New512(), nil },
-		"sha512_256":  func() (hash.Hash, error) { return sha512.New512_256(), nil },
+		// MD Family
+		"md4": func() (hash.Hash, error) { return md4.New(), nil },
+		"md5": func() (hash.Hash, error) { return md5.New(), nil },
+
+		// SHA-1 Family
+		"sha1": func() (hash.Hash, error) { return sha1.New(), nil },
+
+		// SHA-2 Family
+		"sha256":     func() (hash.Hash, error) { return sha256.New(), nil },
+		"sha224":     func() (hash.Hash, error) { return sha256.New224(), nil },
+		"sha384":     func() (hash.Hash, error) { return sha512.New384(), nil },
+		"sha512_224": func() (hash.Hash, error) { return sha512.New512_224(), nil },
+		"sha512":     func() (hash.Hash, error) { return sha512.New(), nil },
+		"sha512_256": func() (hash.Hash, error) { return sha512.New512_256(), nil },
+
+		// SHA-3 Family
+		"sha3_224": func() (hash.Hash, error) { return sha3.New224(), nil },
+		"sha3_256": func() (hash.Hash, error) { return sha3.New256(), nil },
+		"sha3_384": func() (hash.Hash, error) { return sha3.New384(), nil },
+		"sha3_512": func() (hash.Hash, error) { return sha3.New512(), nil },
+
+		// RIPEMD
+		"ripemd160": func() (hash.Hash, error) { return ripemd160.New(), nil },
+
+		// BLAKE2 Family
 		"blake2s_256": func() (hash.Hash, error) { return blake2s.New256(nil) },
 		"blake2b_256": func() (hash.Hash, error) { return blake2b.New256(nil) },
 		"blake2b_384": func() (hash.Hash, error) { return blake2b.New384(nil) },
@@ -41,9 +52,8 @@ func GetHashFunc() map[string]func() (hash.Hash, error) {
 }
 
 func HashData(message, hashName string) (string, error) {
-	formattedHashName := strings.ToLower(hashName)
 	hashList := GetHashFunc()
-	hashFunc, exists := hashList[formattedHashName]
+	hashFunc, exists := hashList[strings.ToLower(hashName)]
 	if !exists {
 		return "", fmt.Errorf("unsupported hash algorithm: %s", hashName)
 	}
